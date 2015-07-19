ee3c5d1eaaa27ad631bacc1211b392569e022fbf
robertbindar <robertbindar@gmail.com>
2015-07-18 22:46:03 -0700

    basic functionality for push api

---
+++ push-notifications-api-demo/js/push-notifications.js
@@ -0,0 +1,31 @@
+
+function registerWorker() {
+    navigator.serviceWorker.register("/js/sw.js", {scope: "/js/"}).then(
+            function(sw) {
+                requestPermission();
+            },
+            function(err) {
+                console.log(err);
+            }
+    );
+}
+
+function requestPermission() {
+    Notification.requestPermission(function() {
+        if (permission === "granted") {
+            navigator.serviceWorker.ready.then(function(sw) {
+                var push_manager = sw.pushManager;
+                push_manager.subscribe({userVisibileOnly: true}).then(function(ps) {
+                    console.log(JSON.stringify(ps));
+                    var endpoint = ps.endpoint;
+                    console.log(endpoint);
+                }
+            });
+
+            return;
+        }
+
+        console.log("Notification.requestpermission denied");
+    });
+}
+
---
+++ push-notifications-api-demo/js/sw.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var baseUrl = new URL("/", this.location.href) + "";
+
+this.addEventListener("install", function(evt) {
+    console.log("SW oninstall");
+});
+
+this.addEventListener("activate", function(evt) {
+    console.log("SW onactivate");
+    evt.waitUntil(self.clients.claim());
+});
+
+this.addEventListener('push', function(evt) {
+    var title = "new Notification";
+    var options = {
+        body: 'test body',
+        tag: 'testtag'
+    };
+
+    self.registration.showNotification(title, options);
+});
+
+this.addEventListener('notificationclick', function(evt) {
+    console.log("SW notificationclick");
+    evt.notification.close();
+
+    // Enumerate windows, and call window.focus(), or open a new one.
+    evt.waitUntil(clients.matchAll({
+        type: "window",
+        includeUncontrolled: true
+    }).then(function(clientList) {
+        //for (var i = 0; i < clientList.length; i++) {
+            //var client = clientList[i];
+            //if (client.focus)
+                //return client.focus();
+        //}
+        //if (clients.openWindow)
+            //return clients.openWindow("/chat/");
+    }));
+});
+
+console.log('Logged from inside SW');
+
