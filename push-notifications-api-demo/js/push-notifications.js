166ddb32b4fa6b58aa76f6069ef8c13fc0db01f5
robertbindar <robertbindar@gmail.com>
2015-07-20 11:45:50 -0700

    send notification to push server (Mozilla's SimplePush)

--- push-notifications-api-demo/js/push-notifications.js
+++ push-notifications-api-demo/js/push-notifications.js
@@ -1,4 +1,6 @@
 
+var g_endpoint;
+
 function registerWorker() {
     navigator.serviceWorker.register("./push-notifications-api-demo/js/sw.js", {scope: "./push-notifications-api-demo/js/"}).then(
             //function(sw) {
@@ -22,16 +24,15 @@ function requestPermission(swRegistration) {
     Notification.requestPermission(function(permission) {
         if (permission === "granted") {
             console.log("permission granted");
-            //navigator.serviceWorker.ready.then(function(sw) {
-                console.log("sw ready: " + swRegistration);
-                var push_manager = swRegistration.pushManager;
-                console.log("push_manager: " + push_manager);
-                push_manager.subscribe({userVisibileOnly: true}).then(function(ps) {
-                    console.log(JSON.stringify(ps));
-                    var endpoint = ps.endpoint;
-                    console.log(endpoint);
-                });
-            //});
+            console.log("sw ready: " + swRegistration);
+            var push_manager = swRegistration.pushManager;
+            console.log("push_manager: " + push_manager);
+            push_manager.subscribe({userVisibileOnly: true}).then(function(ps) {
+                console.log(JSON.stringify(ps));
+                g_endpoint = ps.endpoint;
+                console.log(g_endpoint);
+
+            });
 
             return;
         }
@@ -41,5 +42,20 @@ function requestPermission(swRegistration) {
 }
 
 function sendNotificationToPushService() {
-    // TODO:
+    var data = new FormData();
+    data.append("version=5");
+
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            console.log("notification was successfully send");
+        } else {
+            console.log("received error from push server");
+        }
+    };
+    xhr.onerror = function() {
+        console.log("error while sending to push server endpoint");
+    };
+    xhr.open('PUT', g_endpoint);
+    xhr.send(data);
 }
